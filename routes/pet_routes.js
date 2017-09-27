b510b91f90a8159e5dfe989fa74392369a5e4ddc
Jay Jackson <jaynickjackson@gmail.com>
2017-09-26 22:51:58 -0400

    Added asyncronous request processing

--- routes/pet_routes.js
+++ routes/pet_routes.js
@@ -1,18 +1,44 @@
-let r = require('request').defaults({
-  json: true
-})
-
+let r = require('request').defaults({ json: true })
+let async = require('async')
 
 module.exports = function(app) {
 
   // Read
   app.get('/pets', function(req, res) {
-    r({uri: 'http://localhost:4000/dog'}, function(error, response, body) {
-      if(!error && response.statusCode === 200) {
-        res.json(body)
-      } else {
-        res.send(response.statusCode)
+
+    async.parallel({
+      cats: function(callback) {
+        r({uri: 'http://localhost:3000/cat'}, function(error, response, body) {
+          if(error) {
+            callback({service: 'cat', error: error})
+            return
+          }
+          if(!error && response.statusCode === 200) {
+            callback(null, body)
+          } else {
+            callback(response.statusCode)
+          }
+        })
+      },
+      dogs: function(callback) {
+        r({uri: 'http://localhost:4000/dog'}, function(error, response, body) {
+          if(error) {
+            callback({service: 'dog', error: error})
+            return
+          }
+          if(!error && response.statusCode === 200) {
+            callback(null, body)
+          } else {
+            callback(response.statusCode)
+          }
+        })
       }
+    }, 
+    function(error, results){
+      res.json({
+        error: error,
+        results: results
+      })
     })
   })
 }
