ed3ad544611fa845eb4c2105b106e6db9114c94f
Jay Jackson <jaynickjackson@gmail.com>
2017-09-26 23:52:22 -0400

    reduced lines using fat arrows and ternaries

--- cat_routes.js
+++ cat_routes.js
@@ -4,58 +4,30 @@ let Cat = require('./cat_model')
 module.exports = function(app) {
 
   // Create
-  app.post('/cat', function(req, res) {
-    let newCat = new Cat(req.body)
-    newCat.save(function(err) {
-      if(err) {
-        res.json({info: 'error during cat create', error: err})
-      } else {
-        res.json({info: 'cat created successfully'})
-      }
-    })
+  app.post('/cat', (req, res) => {
+    newCat = new Cat(req.body)
+    newCat.save((err) => { err ? res.json({info: 'error during cat create', error: err}) : res.json({info: 'cat created successfully'}) })
   })
 
   // Read All
-  app.get('/cat', function(req, res) {
-    Cat.find(function(err, cats) {
-      if(err) {
-        res.json({ info: 'error during find cats', error: err })
-      } else {
-        res.json({ info: 'cats found successfully', data: cats })
-      }
-    })
+  app.get('/cat', (req, res) => {
+    Cat.find((err, cats) => { err ? res.json({ info: 'error during find cats', error: err }) : res.json({ info: 'cats found successfully', data: cats })})
   })
 
   // Read One
-  app.get('/cat/:id', function(req, res) {
-    Cat.findById(req.params.id, function(err, cat) {
-      if(err) {
-        res.json({ info: 'error during find cat', error: err })
-      } else {
-        if(cat) {
-          res.json({ info: 'cat found successfully', data: cat })
-        } else {
-          res.json({ info: 'cat not found' })
-        }
-      }
-    })
+  app.get('/cat/:id', (req, res) =>{
+    Cat.findById(req.params.id, (err, cat) => { err ? res.json({ info: 'error during find cat', error: err }) : cat ? res.json({ info: 'cat found successfully', data: cat }) : res.json({ info: 'cat not found' })})
   })
 
   // Update
-  app.put('/cat/:id', function(req, res) {
-    Cat.findById(req.params.id, function(err, cat) {
+  app.put('/cat/:id', (req, res) => {
+    Cat.findById(req.params.id, (err, cat) => {
       if(err) {
         res.json({ info: 'error during find cat', error: err })
       } else {
         if(cat) {
           _.merge(cat, req.body)
-          cat.save(function(err) {
-            if(err) {
-              res.json({ info: 'error during cat update', error: err })
-            } else {
-              res.json({ info: 'cat updated successfully' })
-            }
-          })
+          cat.save((err) => { err ? res.json({ info: 'error during cat update', error: err }) : res.json({ info: 'cat updated successfully' })})
         } else {
           res.json({ info: 'cat not found' })
         }
@@ -64,14 +36,7 @@ module.exports = function(app) {
   })
 
   // Delete
-  app.delete('/cat/:id', function(req, res){
-    Cat.findByIdAndRemove(req.params.id, function(err) {
-      if(err) {
-        res.json({ info: 'error during remove cat', error: err})
-      } else {
-        res.json({info: 'cat removed successfully'})
-      }
-      
-    })
+  app.delete('/cat/:id', (req, res) => {
+    Cat.findByIdAndRemove(req.params.id, (err) => { err ? res.json({ info: 'error during remove cat', error: err}) : res.json({info: 'cat removed successfully'})})
   })
 }
--- index.js
+++ index.js
@@ -6,12 +6,7 @@ let mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/cats')
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
+app.use(bodyParser.urlencoded({extended: true}))
 
 let cats = require('./cat_routes')(app)
-
-let server = app.listen(3000, function(){
-  console.log('Listening on port 3000')
-})
+let server = app.listen(3000, () => console.log('Listening on port 3000'))
